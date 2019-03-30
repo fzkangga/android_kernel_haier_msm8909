ec7e5243447e4dbcd98c2a979eef39ec7973faf1
fzkangga <titahpriyangga@gmail.com>
2019-03-30 12:00:55 +0700

    Revert "msm: camera: isp: Enable regulator to turn on clocks in ISPIF."
    This reverts commit 6bddddf44817ede70a4b461029247bec5275f5fe.

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -79,57 +79,6 @@ static struct msm_cam_clk_info ispif_8626_reset_clk_info[] = {
 	{"camss_csi_vfe0_clk", NO_SET_RATE},
 };
 
-static int msm_ispif_set_regulator(struct ispif_device *ispif_dev,
-	uint8_t enable)
-{
-	int rc = 0;
-
-	if (enable) {
-		if (!ispif_dev->fs_vfe0) {
-			ispif_dev->fs_vfe0 = regulator_get(
-				&ispif_dev->pdev->dev, "vdd");
-			if (IS_ERR_OR_NULL(ispif_dev->fs_vfe0)) {
-				pr_err("%s: Regulator vfe0 get failed %ld\n",
-					__func__,
-					PTR_ERR(ispif_dev->fs_vfe0));
-				rc = -ENODEV;
-				goto vfe0_reg_get_failed;
-			}
-		}
-		if (ispif_dev->fs_vfe0) {
-			rc = regulator_enable(ispif_dev->fs_vfe0);
-			if (rc) {
-				pr_err("%s: Regulator enable for vfe0 failed\n",
-					__func__);
-				goto fs_vfe0_en_failed;
-			}
-		}
-	} else {
-		if (ispif_dev->fs_vfe0) {
-			regulator_disable(ispif_dev->fs_vfe0);
-			regulator_put(ispif_dev->fs_vfe0);
-			ispif_dev->fs_vfe0 = NULL;
-		}
-		if (ispif_dev->fs_camss) {
-			regulator_disable(ispif_dev->fs_camss);
-			regulator_put(ispif_dev->fs_camss);
-			ispif_dev->fs_camss = NULL;
-		}
-		if (ispif_dev->fs_mmagic_camss) {
-			regulator_disable(ispif_dev->fs_mmagic_camss);
-			regulator_put(ispif_dev->fs_mmagic_camss);
-			ispif_dev->fs_mmagic_camss = NULL;
-		}
-	}
-	return rc;
-
-fs_vfe0_en_failed:
-	regulator_put(ispif_dev->fs_vfe0);
-	ispif_dev->fs_vfe0 = NULL;
-vfe0_reg_get_failed:
-	return rc;
-}
-
 static struct msm_cam_clk_info ispif_8974_ahb_clk_info[ISPIF_CLK_INFO_MAX];
 
 static struct msm_cam_clk_info ispif_8974_reset_clk_info[] = {
@@ -164,13 +113,6 @@ static int msm_ispif_reset_hw(struct ispif_device *ispif, int release)
 	struct clk *reset_clk[ARRAY_SIZE(ispif_8974_reset_clk_info)];
 	struct clk *reset_clk1[ARRAY_SIZE(ispif_8626_reset_clk_info)];
 	ispif->clk_idx = 0;
-
-	/* Turn ON regulators before enabling the clocks*/
-	rc = msm_ispif_set_regulator(ispif, 1);
-	if (rc < 0) {
-		pr_err("%s: ispif enable regulator failed", __func__);
-			return -EFAULT;
-	}
 
 	rc = msm_cam_clk_enable(&ispif->pdev->dev,
 		ispif_8974_reset_clk_info, reset_clk,
@@ -230,8 +172,6 @@ static int msm_ispif_reset_hw(struct ispif_device *ispif, int release)
 				pr_err("%s: VFE0 reset wait timeout\n",
 					 __func__);
 		}
-		/* Turn OFF regulators */
-		rc = msm_ispif_set_regulator(ispif, 0);
 		return -ETIMEDOUT;
 	}
 
@@ -245,8 +185,6 @@ static int msm_ispif_reset_hw(struct ispif_device *ispif, int release)
 			msm_cam_clk_enable(&ispif->pdev->dev,
 				ispif_8974_reset_clk_info, reset_clk,
 				ARRAY_SIZE(ispif_8974_reset_clk_info), 0);
-			/* Turn OFF regulators */
-			rc = msm_ispif_set_regulator(ispif, 0);
 			return -ETIMEDOUT;
 		}
 	}
@@ -269,13 +207,6 @@ static int msm_ispif_reset_hw(struct ispif_device *ispif, int release)
 			pr_err("%s: cannot disable clock, error = %d",
 				__func__, rc);
 		}
-	}
-
-	/* Turn OFF regulators after enabling the clocks*/
-	rc = msm_ispif_set_regulator(ispif, 0);
-	if (rc < 0) {
-		pr_err("%s: ispif disable regulator failed", __func__);
-		return -EFAULT;
 	}
 
 	return rc;
@@ -913,12 +844,6 @@ static int msm_ispif_restart_frame_boundary(struct ispif_device *ispif,
 		vfe_mask |= (1 << vfe_intf);
 	}
 
-	/* Turn ON regulators before enabling the clocks*/
-	rc = msm_ispif_set_regulator(ispif, 1);
-	if (rc < 0) {
-		pr_err("%s: ispif enable regulator failed", __func__);
-		return -EFAULT;
-	}
 	rc = msm_cam_clk_enable(&ispif->pdev->dev,
 		ispif_8974_reset_clk_info, reset_clk,
 		ARRAY_SIZE(ispif_8974_reset_clk_info), 1);
@@ -929,7 +854,6 @@ static int msm_ispif_restart_frame_boundary(struct ispif_device *ispif,
 		if (rc < 0) {
 			pr_err("%s: cannot enable clock, error = %d",
 				__func__, rc);
-			goto disable_regulator;
 		} else {
 			/* This is set when device is 8x26 */
 			ispif->clk_idx = 2;
@@ -983,7 +907,7 @@ static int msm_ispif_restart_frame_boundary(struct ispif_device *ispif,
 		if (rc < 0) {
 			pr_err("%s: cannot disable clock, error = %d",
 				__func__, rc);
-			goto disable_regulator;
+			goto end;
 		}
 	}
 
@@ -994,16 +918,8 @@ static int msm_ispif_restart_frame_boundary(struct ispif_device *ispif,
 		if (rc < 0) {
 			pr_err("%s: cannot disable clock, error = %d",
 				__func__, rc);
-			goto disable_regulator;
+			goto end;
 		}
-	}
-
-	/* Turn OFF regulators after disabling the clocks*/
-	rc = msm_ispif_set_regulator(ispif, 0);
-	if (rc < 0) {
-		pr_err("%s: ispif disable regulator failed", __func__);
-		rc = -EFAULT;
-		goto end;
 	}
 
 	for (i = 0; i < params->num; i++) {
@@ -1050,8 +966,8 @@ static int msm_ispif_restart_frame_boundary(struct ispif_device *ispif,
 			cid_mask, vfe_intf, 1);
 	}
 
+end:
 	return rc;
-
 disable_clk:
 	rc = msm_cam_clk_enable(&ispif->pdev->dev,
 		ispif_8974_reset_clk_info, reset_clk,
@@ -1060,12 +976,12 @@ disable_clk:
 		rc = msm_cam_clk_enable(&ispif->pdev->dev,
 			ispif_8626_reset_clk_info, reset_clk1,
 			ARRAY_SIZE(ispif_8626_reset_clk_info), 0);
+		if (rc < 0)
+			pr_err("%s: cannot enable clock, error = %d",
+				__func__, rc);
 	}
-disable_regulator:
-	/* Turn OFF regulators */
-	msm_ispif_set_regulator(ispif, 0);
-end:
-	return rc;
+
+	return -ETIMEDOUT;
 }
 
 static int msm_ispif_stop_frame_boundary(struct ispif_device *ispif,
